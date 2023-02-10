9dff9190bfce246926b3f462bc3997e330fbbec1
jskrn120 <js516718153@gmail.com>
2023-02-10 10:04:55 -0700

    pages linked

--- app/src/main/java/com/example/datingapp/SignUp.java
+++ app/src/main/java/com/example/datingapp/SignUp.java
@@ -1,7 +1,9 @@
 package com.example.datingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public void onClickSignUp(View v){
+        Intent intent = new Intent(this, Questions_page.class);
+        startActivity(intent);
     }
 }
