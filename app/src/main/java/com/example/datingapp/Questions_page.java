fb8bd58c154634a734f915db7df6f425be055848
jskrn120 <js516718153@gmail.com>
2023-02-16 20:37:30 -0700

    Revert "ddvdx"
    This reverts commit e4ed401417ded41fde796b96e106278278421436.

--- app/src/main/java/com/example/datingapp/Questions_page.java
+++ app/src/main/java/com/example/datingapp/Questions_page.java
@@ -2,9 +2,6 @@ package com.example.datingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 public class Questions_page extends AppCompatActivity {
 
@@ -12,11 +9,5 @@ public class Questions_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page);
-
-
-            }
-
-
-
-
+    }
 }
