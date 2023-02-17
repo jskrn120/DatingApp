62b3aa9252bf77a37fbd26f6cd944126fab73353
jskrn120 <js516718153@gmail.com>
2023-02-16 23:46:26 -0700

    reset

--- app/src/main/java/com/example/datingapp/Questions_page.java
+++ app/src/main/java/com/example/datingapp/Questions_page.java
@@ -1,11 +1,7 @@
 package com.example.datingapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-
-import java.util.prefs.Preferences;
 
 public class Questions_page extends AppCompatActivity {
 
@@ -13,9 +9,5 @@ public class Questions_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page);
-    }
-    public void onClickLogIn(View v){
-        Intent intent = new Intent(this, Preferences.class);
-        startActivity(intent);
     }
 }
