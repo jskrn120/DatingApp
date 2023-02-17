748e0088d6dcc24074216dec024e9151833c25a3
jskrn120 <js516718153@gmail.com>
2023-02-16 20:33:25 -0700

    Revert "Merge branch 'master' of https://github.com/jskrn120/DatingApp"
    This reverts commit b9b7d61c59ab6b74b83751dd455beb87b928ff3e, reversing
    changes made to 40c2d86bd767799e501279a6bdc6b086b6f84187.

--- app/src/main/java/com/example/datingapp/Questions_page.java
+++ app/src/main/java/com/example/datingapp/Questions_page.java
@@ -1,11 +1,10 @@
 package com.example.datingapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-
-import java.util.prefs.Preferences;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Questions_page extends AppCompatActivity {
 
@@ -13,9 +12,11 @@ public class Questions_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page);
-    }
-    public void onClickLogIn(View v){
-        Intent intent = new Intent(this, Preferences.class);
-        startActivity(intent);
-    }
+
+
+            }
+
+
+
+
 }
